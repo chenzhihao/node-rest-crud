bb2375e2d56cd09242b29db092aaa2a7d6736135
chenzhihao <cabtravis@gmail.com>
2015-12-21 13:18:35 +0800

    CRUD routes for todo model.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ router.get('/', function (req, res) {
   res.render('index', {title: 'Express'});
 });
 
-router.route('/todos').get(todoRoute.get).post(todoRoute.post);
+router.route('/todos').get(todoRoute.getTodos).post(todoRoute.createTodo);
+
+router.route('/todos/:id').get(todoRoute.getTodo).delete(todoRoute.deleteTodo);
 
 module.exports = router;
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,15 +1,69 @@
 const Todo = require('../models/todoModel');
 
 module.exports = {
-    get(req, res) {
-        Todo.collection().fetch().then((todos)=>{
-            res.json(todos);
-        });
-    },
+  getTodos(req, res) {
+    Todo.collection().fetch().then((todos)=> {
+      res.json({
+        status: 'ok',
+        data: todos.toJSON()
+      });
+    });
+  },
 
-    post(req,res) {
-      new Todo({text:'text'}).save().then((model)=> {
-          res.json({ok: true});
+  createTodo(req, res) {
+    const text = req.body.text;
+    if (text) {
+      Todo.forge({
+        text
+      }).save().then((model)=> {
+        res.json({
+          status: 'ok',
+          data: model.toJSON()
+        });
+      });
+    } else {
+      res.json({
+        status: `"text" field is required`,
+        data: null
       });
     }
+  },
+
+  getTodo(req, res) {
+    const id = req.params.id;
+    Todo.forge({
+      id
+    }).fetch().then((model)=> {
+      res.json({
+        status: 'ok',
+        data: model.toJSON()
+      });
+    }).catch((err)=> {
+        res.status(404).json({
+          status: 'error',
+          error: err,
+          data: null
+        });
+      }
+    );
+  },
+
+  deleteTodo(req, res) {
+    const id = req.params.id;
+    Todo.forge({
+      id
+    }).destroy({require: true}).then((model)=> {
+      res.json({
+        status: 'ok',
+        data: model.toJSON()
+      });
+    }).catch((err)=> {
+        res.status(404).json({
+          status: 'error',
+          error: err,
+          data: null
+        });
+      }
+    );
+  }
 };
